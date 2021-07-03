85821bc21acc0dfa79e11c1c6e6b1a9688fbc7cd
양효빈 <qls0786@naver.com>
2021-07-03 13:59:20 +0000

    send, recv 함수 분리

--- client.py
+++ client.py
@@ -1,26 +1,30 @@
 from socket import *
 
+def send(sock):
+    sendData = input('당신 : ')
+    sock.send(sendData.encode('utf-8'))
+    if(sendData == 'quit'):
+        print('대화방을 나갑니다.')
+        return 1
+def recv(sock):
+    recvData = (sock.recv(1024)).decode('utf-8')
+    if(recvData == 'quit'):
+        print('상대방이 대화방을 나갔습니다.')
+        return 1
+
+    print('상대방 :', recvData)
+
+
 print('------client------')
 clientSock=socket(AF_INET, SOCK_STREAM)
-clientSock.connect(('127.0.0.1',8104))
+clientSock.connect(('127.0.0.1',8106))
 
 print('Connected...!')
 
 
 while(True):
-    
-    recvData=(clientSock.recv(1024)).decode('utf-8')
-
-    if(recvData=='quit'):
-        print('상대방이 대화방을 나갔습니다.')
+ 
+    if(recv(clientSock) or send(clientSock)):
         break
-    
-    print('상대방 : ', recvData)
 
-    sendData=input('당신 : ')
-    
-    clientSock.send(sendData.encode('utf-8'))
-    if(sendData=='quit'):
-        break
-    
 clientSock.close()
