242e613afe389556193f565312f0796e70fa7696
양효빈 <qls0786@naver.com>
2021-07-04 14:24:05 +0000

    연결 종료(quit 구문) 수정

--- client.py
+++ client.py
@@ -1,20 +1,27 @@
 from socket import *
 from threading import *
+import sys
 import time
 
 def send(sock):
-    sendData = input('당신 : ')
-    sock.send(sendData.encode('utf-8'))
-    if(sendData == 'quit'):
-        print('대화방을 나갑니다.')
-        return 1
+    while(True):
+        sendData = input('당신 : ')
+        sock.send(sendData.encode('utf-8'))
+        if(sendData == 'quit'):
+            print('대화방을 나갑니다.')
+            clientSock.close()
+            sys.exit(1)
+        
 def recv(sock):
-    recvData = (sock.recv(1024)).decode('utf-8')
-    if(recvData == 'quit'):
-        print('상대방이 대화방을 나갔습니다.')
-        return 1
+    while(True):
+        recvData = (sock.recv(1024)).decode('utf-8')
+        if(recvData == 'quit'):
+            print('상대방이 대화방을 나갔습니다.')
+            clientSock.close()
+            sys.exit(1)
 
-    print('상대방 :', recvData)
+        print('상대방 :', recvData)
+    return 1
 
 
 print('------client------')
@@ -26,6 +33,10 @@ print('Connected...!')
 
 sender = Thread(target=send, args=(clientSock,))
 receiver = Thread(target=recv, args=(clientSock,))
+'''
+sender.daemon=True
+receiver.daemon=True
+'''
 
 sender.start()
 receiver.start()
@@ -33,4 +44,5 @@ receiver.start()
 while True:
     time.sleep(1)
     pass
-clientSock.close()
+
+
--- server.py
+++ server.py
@@ -1,24 +1,26 @@
 from socket import *
 from threading import *
+import sys
 import time
 
 def send(sock):
-    quit_check=1
-    while(quit_check):
+    while(True):
         sendData = input('당신 : ')
         sock.send(sendData.encode('utf-8'))
         if(sendData == 'quit'):
-            quit_check=0
-    print('대화방을 나갑니다.')
-    return 1
+            print('대화방을 나갑니다.')
+            serverSock.close()
+            sys.exit(1)
 
 def recv(sock):
     while(True):
         recvData = (sock.recv(1024)).decode('utf-8')
         if(recvData == 'quit'):
-            break
+            print('상대방이 대화방을 나갔습니다.')
+            serverSock.close()
+            sys.exit(1)
+            
         print('상대방 :', recvData)
-    print('상대방이 대화방을 나갔습니다.')
     return 1
 
     
@@ -26,7 +28,9 @@ def recv(sock):
 
 print('------Server------')
 serverSock = socket(AF_INET, SOCK_STREAM)
+serverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 serverSock.bind(('127.0.0.1', 8106))
+
 serverSock.listen(1)
 
 print('연결 대기중...')
@@ -35,7 +39,13 @@ connectionSock, addr = serverSock.accept()
 print(str(addr), '와 연결되었습니다!')
 
 sender = Thread(target=send, args=(connectionSock,))
+
 receiver = Thread(target=recv, args=(connectionSock,))
+
+'''
+sender.daemon=True
+receiver.daemon=True
+'''
 
 sender.start()
 receiver.start()
@@ -43,3 +53,5 @@ receiver.start()
 while True:
     time.sleep(1)
     pass
+
+
